3028afa8c9b0b7faa9ac6ecfafafa7993eaffdd2
Native <konstvl9@gmail.com>
2024-07-26 12:25:09 +1000

    refactoring

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"tgBot/bot"
 )
 
 func main() {
-	bot.MainBot()
+	newBot, err := bot.NewBot()
+	if err != nil {
+		log.Fatal(fmt.Errorf("new bot: %w", err))
+	}
+
+	startedBot, err := newBot.StartBot()
+	if err != nil {
+		log.Fatal(fmt.Errorf("start bot: %w", err))
+	}
+
+	fmt.Println("Bot is now running.")
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	<-interrupt
+
+	fmt.Println("Bot is shutting down.")
+
+	startedBot.Stop()
 }
